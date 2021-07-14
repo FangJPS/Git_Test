7c34f295e353b6cb93bba9c5c73b0edfb91a37db
jps <jps@qq.com>
2021-07-14 17:08:28 +0800

    master first

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,5 +3,6 @@ package org.example;
 public class Test {
     public static void main(String[] args) {
         System.out.println("热的第二次");
+        System.out.println("master  第一次人造冲突");
     }
 }
